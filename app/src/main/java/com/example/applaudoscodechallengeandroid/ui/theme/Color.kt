207e889747969f99adcba5e0343230c014f32632
Elvis Antigua <elvisantiguam@gmail.com>
2023-01-04 17:14:09 -0400

    Added dependencies and theme configuration

--- app/src/main/java/com/example/applaudoscodechallengeandroid/ui/theme/Color.kt
+++ app/src/main/java/com/example/applaudoscodechallengeandroid/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.example.applaudoscodechallengeandroid.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple = Color(0xFF6243FF)
+val Blue = Color(0xFF0013CA)
+val LightBlue = Color(0xFF32B4FF)
+val Turquoise = Color(0xFF21BDCA)
+val LightTurquoise = Color(0xFF84DFE2)
+val Black = Color(0xFF25294A)
+val White = Color(0xFFFBFAFE)
+val Gray = Color(0xFFD5D8DB)
+val Red = Color(0xFFF65164)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Background = Color(0xFFF0F2F5)
+val Text = Color(0xFF6B6B83)
+val SubText = Color(0xFF8C8CA1)
